30ec90d799c61f993e975d0e45e047e5dc8aa2a6
MaxDu <maximilianjdu@gmail.com>
2023-03-20 18:34:11 -0700

    cleanup3

--- experiments/exps.sh
+++
@@ -1,7 +0,0 @@
-#python policy_gradient.py --seed 20  --level 2 --name "full_algorithm" --eval_trials 20 --eval_frq 5 --batch_size 10 --epochs 100  --exp_dir "experiments/initial_tests" --shaped_reward --counterfactual --object_prior
-python policy_gradient.py --seed 20  --level 4 --name "full_algorithm" --eval_trials 20 --eval_frq 20 --batch_size 1 --epochs 200  --exp_dir "experiments/initial_tests"  --shaped_reward --counterfactual --object_prior
-python policy_gradient.py --seed 20  --level 0 --name "full_algorithm" --eval_trials 20 --eval_frq 20 --batch_size 1 --epochs 40  --exp_dir "experiments/initial_tests" --shaped_reward --counterfactual --object_prior
-python policy_gradient.py --seed 20  --level 3 --name "full_algorithm" --eval_trials 20 --eval_frq 20 --batch_size 1 --epochs 200  --exp_dir "experiments/initial_tests"  --shaped_reward --counterfactual --object_prior
-python policy_gradient.py --seed 2  --name "ablate_prior" --eval_trials 20 --eval_frq 5 --batch_size 10 --epochs 100  --exp_dir "experiments/initial_tests" --shaped_reward --counterfactual
-python policy_gradient.py --seed 2  --name "ablate_reward" --eval_trials 20 --eval_frq 5 --batch_size 10 --epochs 100  --exp_dir "experiments/initial_tests" --counterfactual --object_prior
-python policy_gradient.py --seed 2  --name "ablate_counterfactual" --eval_trials 20 --eval_frq 5 --batch_size 10 --epochs 100  --exp_dir "experiments/initial_tests" --object_prior --shaped_reward
